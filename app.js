79d9a494e3de06db2051a704fa79ad40b8fcc75f
Richard Glander <yggdrasilprecision@outlook.de>
2017-06-09 13:31:42 +0200

    webpack init

--- app.js
+++ app.js
@@ -1,36 +1,36 @@
 var app = angular.module('demo', []);
 
 app.controller('comments', function($scope, $http) {
-    $http.get('https://jsonplaceholder.typicode.com/comments').
-	then(function(response){
-	    $scope.comments = response.data;
-	});
+  $http.get('https://jsonplaceholder.typicode.com/comments').
+  then(function(response) {
+    $scope.comments = response.data;
+  });
 });
 app.controller('albums', function($scope, $http) {
-    $http.get('https://jsonplaceholder.typicode.com/albums').
-	then(function(response){
-	    $scope.albums = response.data;
-	});
+  $http.get('https://jsonplaceholder.typicode.com/albums').
+  then(function(response) {
+    $scope.albums = response.data;
+  });
 });
 app.controller('photos', function($scope, $http) {
-    $http.get('https://jsonplaceholder.typicode.com/photos').
-	then(function(response){
-	    $scope.photos = response.data;
-	});
+  $http.get('https://jsonplaceholder.typicode.com/photos').
+  then(function(response) {
+    $scope.photos = response.data;
+  });
 });
 app.controller('todos', function($scope, $http) {
-    $http.get('https://jsonplaceholder.typicode.com/todos').
-	then(function(response){
-	    $scope.todos = response.data;
-	});
+  $http.get('https://jsonplaceholder.typicode.com/todos').
+  then(function(response) {
+    $scope.todos = response.data;
+  });
 });
 app.controller('users', function($scope, $http) {
-  $scope.sortType     = 'name'; // set the default sort type
-$scope.sortReverse  = false;  // set the default sort order
-$scope.searchUser   = '';     // set the default search/filter term
+  $scope.sortType = 'name'; // set the default sort type
+  $scope.sortReverse = false; // set the default sort order
+  $scope.searchUser = ''; // set the default search/filter term
 
-    $http.get('https://jsonplaceholder.typicode.com/users').
-	then(function(response){
-	    $scope.users = response.data;
-	});
+  $http.get('https://jsonplaceholder.typicode.com/users').
+  then(function(response) {
+    $scope.users = response.data;
+  });
 });
