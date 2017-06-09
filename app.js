aaa801d37293856832bde8944a31592c4d96544e
Richard Glander <yggdrasilprecision@outlook.de>
2017-06-09 10:30:22 +0200

    added controller into app.js for testing purposes

--- app.js
+++ app.js
@@ -1,10 +1,43 @@
 var app = angular.modul("demo", ["ngRoute"]);
-app.config(function($routeProvider){
-    $routeProvider.when("/",{
-      templateUrl : "home.html",
-      controller: "homeCtrl"
-    }).when("/users",{
-      templateUrl : "users.html",
-      controller : "usersCtrls"
-    })
+app.config(function($routeProvider) {
+  $routeProvider.when("/", {
+    templateUrl: "app/templates/home.html",
+    controller: "usersCtrl"
+  }).when("/users", {
+    templateUrl: "users.html",
+    controller: "usersCtrl"
+  })
+});
+app.controller('comments', function($scope, $http) {
+  $http.get('https://jsonplaceholder.typicode.com/comments').
+  then(function(response) {
+    $scope.comments = response.data;
+  });
+});
+app.controller('albums', function($scope, $http) {
+  $http.get('https://jsonplaceholder.typicode.com/albums').
+  then(function(response) {
+    $scope.albums = response.data;
+  });
+});
+app.controller('photos', function($scope, $http) {
+  $http.get('https://jsonplaceholder.typicode.com/photos').
+  then(function(response) {
+    $scope.photos = response.data;
+  });
+});
+app.controller('todos', function($scope, $http) {
+  $http.get('https://jsonplaceholder.typicode.com/todos').
+  then(function(response) {
+    $scope.todos = response.data;
+  });
+});
+app.controller('usersCtrl', function($scope, $http) {
+  $scope.sortType = 'name'; // set the default sort type
+  $scope.sortReverse = false; // set the default sort order
+  $scope.searchUser = ''; // set the default search/filter term
+  $http.get('https://jsonplaceholder.typicode.com/users').
+  then(function(response) {
+    $scope.users = response.data;
+  });
 });
